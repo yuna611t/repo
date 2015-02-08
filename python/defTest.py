1e07a70549311e36510e2b8b3dbae43f72c4d5d8
naganuma <naganuma@192.168.11.12>
2015-02-08 17:13:37 +0900

    defTest  param def

--- python/defTest.py
+++ python/defTest.py
@@ -1,7 +1,21 @@
 # -*- coding: utf-8 -*-
 
-def test():
-    print "coll test"
+def test(num_1,num_2,operation):
+    
+    if operation == 1:
+        print "足し算開始"
+        print num_1 + num_2
+    elif operation == 2:
+        print "引き算開始"
+        print num_1 - num_2
+    elif operation == 3:
+        print "かけ算開始"
+        print num_1 * num_2 
+    elif operation == 4:
+        print "割り算開始"
+        print num_1 / num_2
+    else:
+        print "不明なオペレーション"
 
 if __name__ == "__main__":
-    test()
+    test(100,10,3)
