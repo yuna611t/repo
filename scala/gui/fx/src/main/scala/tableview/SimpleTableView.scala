f6ebe090004c331bde3080fddacf074688cd0c31
naganuma <naganuma@192.168.11.12>
2015-05-03 19:53:30 +0900

    ScalaFX SimpleTable Sort

--- scala/gui/fx/src/main/scala/tableview/SimpleTableView.scala
+++ scala/gui/fx/src/main/scala/tableview/SimpleTableView.scala
@@ -11,25 +11,33 @@ object SimpleTableView extends JFXApp {
 
 	val characters = ObservableBuffer[Person] (
 		new Person("Peggy", "Sue", "555-6789"),
-		new Person("Rocky", "Raccoon", "555-6798")
+		new Person("Rocky", "Raccoon", "555-6798"),
+		new Person("Desmond", "Sue", "555-8036"),
+		new Person("Molly", "RAccoon", "555-0789")
 	)
 
 	stage = new PrimaryStage {
 		title = "Simple Table View"
 		scene = new Scene {
+			val firstNameColumn = new TableColumn[Person, String] {
+				text = "First Name"
+				cellValueFactory = {_.value.firstName}
+				prefWidth = 180
+			}
+			val lastNameColumn = new TableColumn[Person, String] {
+				text = "Last Name"
+				cellValueFactory = {_.value.lastName}
+				prefWidth = 180
+			}
+			val phoneColumn = new TableColumn[Person, String] {
+				text = "phone"
+				cellValueFactory = {_.value.phone}
+				prefWidth = 180
+			}
+
 			content = new TableView[Person](characters) {
-				columns ++= List(
-					new TableColumn[Person, String] {
-						text = "First Name"
-						cellValueFactory = {_.value.firstName}
-						prefWidth = 180
-					},
-					new TableColumn[Person, String] {
-						text = "Last Name"
-						cellValueFactory = {_.value.lastName}
-						prefWidth = 180
-					}
-				)
+				columns += (firstNameColumn, lastNameColumn, phoneColumn)
+				sortOrder += (phoneColumn, lastNameColumn, firstNameColumn)
 			}
 		}
 	}
