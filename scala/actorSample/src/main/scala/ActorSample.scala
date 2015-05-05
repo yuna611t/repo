71c3f33a3f639d86e5a952199b64810b6c8bb6e9
naganuma <naganuma@192.168.11.12>
2015-05-05 20:07:05 +0900

    Scala AkkaActor Sample

--- scala/actorSample/src/main/scala/ActorSample.scala
+++ scala/actorSample/src/main/scala/ActorSample.scala
@@ -3,19 +3,39 @@ import akka.actor.Props
 import akka.actor.Actor
 import akka.routing.RoundRobinRouter
 
+/**
+* akka.actorの動きについて勉強
+* sbtからの実行方法
+* sbt 'run-main ActorSample Hello'
+* sbt 'run-main ActorSample World'
+* sbt 'run-main ActorSample What'
+*
+* @author yuna
+*/
 object ActorSample {
 	def main(args: Array[String]) = {
-		val system = ActorSystem("HelloSystem")
-		val router = system.actorOf(Props[HelloActor].withRouter(RoundRobinRouter(4)))
 
-		router ! "Hello"
+		// ActorSystemをsystemという名前で作成
+		val system = ActorSystem("system")
+		// system(ActorSystemにSampleActorを登録
+		val router = system.actorOf(Props[SampleActor].withRouter(RoundRobinRouter(4)))
+
+		// メッセージ送信
+		router ! args(0)
+		// system(ActorSystem)の終了
 		system.shutdown
 	}
 }
 
-class HelloActor extends Actor {
+/**
+* Actorの定義
+* @author yuna
+*/
+class SampleActor extends Actor {
+	// 受け取ったメッセージに対して処理を定義
 	def receive = {
 		case "Hello" => println("Hello World!!")
+		case "World" => println("We are the World!!")
 		case _ => println("Bad World!!")
 	}
 }
