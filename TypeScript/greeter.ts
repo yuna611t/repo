cbd326040f38ff3382c604967f4bfa1c4c759107
naganuma <naganuma@192.168.11.12>
2015-08-02 19:09:06 +0900

    TypeScriptの勉強 Class

--- TypeScript/greeter.ts
+++ TypeScript/greeter.ts
@@ -1,3 +1,10 @@
+class Student {
+    fullname: string;
+    constructor(public firstname, public middleinitial, public lastname) {
+        this.fullname = firstname + " " + middleinitial + " " + lastname;
+    }
+}
+
 interface Person {
     firstname: string;
     lastname: string;
@@ -7,6 +14,6 @@ function greeter(person: Person) {
     return "Hello, " + person.firstname + " " + person.lastname;
 }
 
-var user = {firstname: "Jane", lastname: "User"};
+var user = new Student("Jane", "M.", "User");
 
 document.body.innerHTML = greeter(user);
