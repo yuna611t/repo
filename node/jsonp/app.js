19b2ffdb88e680c91267ebfee115f3887272cef1
naganuma <naganuma@192.168.11.12>
2015-08-28 23:41:30 +0900

    jsonp server on nodejs & jsonp callback client

---
+++ node/jsonp/app.js
@@ -0,0 +1,28 @@
+var http = require('http');
+var url = require('url');
+function cnv(s) {
+    return s.replace(/¥¥/g,'¥¥¥¥').replace(/"/g, '¥¥"');
+}
+
+http.createServer(function(req,res){
+    var query = url.parse(req.url, true).query;
+    var data = [];
+    var callback;
+    for (var key in query) {
+        var val = query[key];
+        if (key == 'callback' && /^[a-zA-Z]+[0-9a-zA-Z]*$/.test(val)) {
+            callback = val;
+        } else {
+            data.push('"' + cnv(key) + '"' + ':' + '"' + cnv(val) + '"');
+        }
+    }
+
+    data = "{" + data.join(',') + "}";
+
+    console.log('Accessed by ' + req.url);
+    console.log('createdJson is ' + data);
+    res.writeHead(200, {'Content-Type':'application/json; charset=utf-8'});
+    res.end( callback ? callback + "(" + data + ");" : data);
+}).listen(8124);
+
+console.log('Server running ...');
