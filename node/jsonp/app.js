0beeb46969059d3f1bca7516564a2ff961267eb5
naganuma <naganuma@192.168.11.12>
2015-08-29 13:11:45 +0900

    $.ajaxに書き換え

--- node/jsonp/app.js
+++ node/jsonp/app.js
@@ -11,20 +11,20 @@ http.createServer(function(req,res){
     var callback;
     for (var key in query) {
         var val = query[key];
-        // if (key == 'callback' && /^[a-zA-Z]+[0-9a-zA-Z]*$/.test(val)) {
-        //     callback = val;
-        // } else {
+        if (key == 'callback' && /^[a-zA-Z]+[0-9a-zA-Z]*$/.test(val)) {
+            callback = val;
+        } else {
             data.push('"' + cnv(key) + '"' + ':' + '"' + cnv(val) + '"');
-        // }
+        }
     }
 
     data = "{" + data.join(',') + "}";
+    var returnData = callback ? callback + "(" + data + ");" : data;
 
     console.log('Accessed by ' + req.url);
-    console.log('createdJson is ' + data);
+    console.log('createdJson is ' + returnData);
     res.writeHead(200, {'Content-Type':'application/json; charset=utf-8'});
-    res.end( "callback" + "(" + data + ");" );
-    // res.end( callback ? callback + "(" + data + ");" : data);
+    res.end(returnData);
 }).listen(8124);
 
 console.log('Server running ...');
