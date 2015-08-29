0b9e5f80030731643655b154d92b6bac253dd03e
naganuma <naganuma@192.168.11.12>
2015-08-29 13:15:26 +0900

    変数局所化

--- node/jsonp/app.js
+++ node/jsonp/app.js
@@ -1,16 +1,22 @@
-var http = require('http');
-var url = require('url');
+var http = require('http'),
+    url = require('url');
+
+
 function cnv(s) {
     console.log(s);
     return s.replace(/¥¥/g,'¥¥¥¥').replace(/"/g, '¥¥"');
 }
 
 http.createServer(function(req,res){
-    var query = url.parse(req.url, true).query;
-    var data = [];
-    var callback;
-    for (var key in query) {
-        var val = query[key];
+    var query = url.parse(req.url, true).query,
+        data = [],
+        callback,
+        returnData,
+        key,val
+    ;
+    
+    for (key in query) {
+        val = query[key];
         if (key == 'callback' && /^[a-zA-Z]+[0-9a-zA-Z]*$/.test(val)) {
             callback = val;
         } else {
@@ -19,7 +25,7 @@ http.createServer(function(req,res){
     }
 
     data = "{" + data.join(',') + "}";
-    var returnData = callback ? callback + "(" + data + ");" : data;
+    returnData = callback ? callback + "(" + data + ");" : data;
 
     console.log('Accessed by ' + req.url);
     console.log('createdJson is ' + returnData);
