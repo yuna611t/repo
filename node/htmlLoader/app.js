d89050b01c9af849a2665cbe91b5d94c092f2e19
naganuma <naganuma@192.168.11.12>
2015-09-06 19:07:41 +0900

    nodejsでurlリストを読み込んでhtmlソースを取得するアプリ(複数行対応)

--- node/htmlLoader/app.js
+++ node/htmlLoader/app.js
@@ -1,26 +1,35 @@
 var fs = require('fs'),
     http = require('http'),
     urlList,
-    body
+    i
 ;
+
+// ファイル削除
+// fs.unlinkSync('./downloadedhtml/*');
+
 
 // URLリスト読み込み
 fs.readFile('./urllist.txt', 'utf8', function (err, str) {
     urlList = str.split(/\r\n|\r|\n/);
+    i = 0;
     // urlごとに処理
-    urlList.forEach(function(url){
-        // 対象URLにアクセス
-        http.get(url, function(res) {
-          res.setEncoding('utf8');
-          res.on('data', function(chunk) {
-            body += chunk;
-            // アクセス先URLのHTMLソースをファイル保存
-            fs.writeFile('./downloadedhtml/writetest.txt', body , function (err) {
-                console.log(err);
-            });
-          });
-        }).on('error', function(e) {
-          console.log("Got error: " + e.message);
-        });
+    urlList.forEach(function(url) {
+        i++;
+        createHTMLSource(url, i);
     });
 });
+
+function createHTMLSource(url, i) {
+    var body = "";
+    // 対象URLにアクセス
+    http.get(url, function(res) {
+        res.setEncoding('utf8');
+        res.on('data', function(chunk) {
+            body += chunk;
+            // アクセス先URLのHTMLソースをファイル保存
+            fs.writeFileSync('./downloadedhtml/'+ i +'.txt', body);
+        });
+    }).on('error', function(e) {
+        fs.writeFileSync('./downloadedhtml/'+ i +'.txt', e);
+    });
+}
