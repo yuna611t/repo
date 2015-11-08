300b544475483f94c04ab824bca3f791e7407c12
naganuma <naganuma@192.168.11.12>
2015-11-08 23:25:36 +0900

    C#の勉強 - Lambda - 引数リスト

--- C#/Sample.cs
+++ C#/Sample.cs
@@ -1,23 +1,9 @@
 using System;
 
-delegate void DelegateAct();
-
 class Program {
-    private static void sample() {
-        Console.WriteLine("QX");
-    }
 
     static void Main(string[] args) {
-        // delegate
-        DelegateAct dAct = sample;
-        dAct();
-
-        // Action
-        Action act = sample;
-        act();
-
-        // Lamda
-        Action lAct = () => Console.WriteLine("QX");
-        lAct();
+        Action<int, int> act = (a,b) => Console.WriteLine("{0}", a + b);
+        act(1, 3);
     }
 }
