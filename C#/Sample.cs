8855187697cd79595a221047ee5bbd1676fb2ec9
naganuma <naganuma@192.168.11.12>
2015-11-08 23:11:16 +0900

    C#の勉強 - 拡張メソッド

--- C#/Sample.cs
+++ C#/Sample.cs
@@ -1,10 +1,29 @@
 using System;
+using Sample2;
 
-class Program {
-    static void Main(string[] args) {
-        long a = 1;
-        Console.WriteLine(a.GetType().Name);
-        var b = 1;
-        Console.WriteLine(b.GetType().Name);
+namespace Sample1 {
+    class A {
+        public void Method1() {
+            Console.WriteLine("in class A");
+        }
+    }
+
+    class Program {
+        static void Main(string[] args) {
+            var x = new A();
+            x.Method1();
+            // class Programからはclass AのMethod1しか見えないはずなのに、
+            // class BのMethod2がclass A経由で支えている
+            x.Method2();
+        }
+    }
+}
+
+namespace Sample2 {
+    static class B {
+        public static void Method2(this Sample1.A a) { // 第一引数はthis と拡張対象の型名
+            a.Method1();
+            Console.WriteLine("in class B");
+        }
     }
 }
