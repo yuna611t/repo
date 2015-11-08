48649b02c3da6133beba8443d1c39b69de93c1e5
naganuma <naganuma@192.168.11.12>
2015-11-08 23:20:18 +0900

    C#の勉強 - Lamda式

--- C#/Sample.cs
+++ C#/Sample.cs
@@ -1,29 +1,23 @@
 using System;
-using Sample2;
 
-namespace Sample1 {
-    class A {
-        public void Method1() {
-            Console.WriteLine("in class A");
-        }
+delegate void DelegateAct();
+
+class Program {
+    private static void sample() {
+        Console.WriteLine("QX");
     }
 
-    class Program {
-        static void Main(string[] args) {
-            var x = new A();
-            x.Method1();
-            // class Programからはclass AのMethod1しか見えないはずなのに、
-            // class BのMethod2がclass A経由で支えている
-            x.Method2();
-        }
-    }
-}
+    static void Main(string[] args) {
+        // delegate
+        DelegateAct dAct = sample;
+        dAct();
 
-namespace Sample2 {
-    static class B {
-        public static void Method2(this Sample1.A a) { // 第一引数はthis と拡張対象の型名
-            a.Method1();
-            Console.WriteLine("in class B");
-        }
+        // Action
+        Action act = sample;
+        act();
+
+        // Lamda
+        Action lAct = () => Console.WriteLine("QX");
+        lAct();
     }
 }
