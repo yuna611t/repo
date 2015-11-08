a880812992f654e58be01cfff428714c9ac3564c
naganuma <naganuma@192.168.11.12>
2015-11-08 22:17:54 +0900

    C#の勉強 - 桁揃え

--- C#/Sample.cs
+++ C#/Sample.cs
@@ -2,6 +2,7 @@ using System;
 
 class Program {
     static void Main(string[] args) {
-        Console.WriteLine("今日{0}さんが{1}円で{2}を書いました", "太郎", 120, "缶コーヒー");
+        Console.WriteLine("{0,3},{1,3},{2,3}", 1, 2, 3);
+        Console.WriteLine("{0,3},{1,3},{2,3}", 123, 456, 789);
     }
 }
