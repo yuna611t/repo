c63d9b705d18a5d8c11d0fc08dd6ea792307f05c
naganuma <naganuma@192.168.11.12>
2015-11-08 22:20:10 +0900

    C#の勉強 - Threading

--- C#/Sample.cs
+++ C#/Sample.cs
@@ -1,8 +1,9 @@
 using System;
+using System.Threading.Tasks;
 
 class Program {
     static void Main(string[] args) {
-        Console.WriteLine("{0,3},{1,3},{2,3}", 1, 2, 3);
-        Console.WriteLine("{0,3},{1,3},{2,3}", 123, 456, 789);
+        Task.Delay(1000).Wait();
+        Console.WriteLine("Hello World!");
     }
 }
