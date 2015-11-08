ad3238271056448d472632743cb86c06028515b1
naganuma <naganuma@192.168.11.12>
2015-11-08 23:29:52 +0900

    C#の勉強 - 式だけのLambda式

--- C#/Sample.cs
+++ C#/Sample.cs
@@ -5,5 +5,8 @@ class Program {
     static void Main(string[] args) {
         Action<int, int> act = (a,b) => Console.WriteLine("{0}", a + b);
         act(1, 3);
+
+        Func<byte, double, int> f = (x, y) => (int)(x * y);
+        Console.WriteLine(f(2,3));
     }
 }
