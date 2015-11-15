d1dbafff5f22c94f5d47345c3a478c746bf0b91f
naganuma <naganuma@192.168.11.12>
2015-11-15 17:47:27 +0900

    C#の勉強 LinQによる数値検証

--- C#/Sample.cs
+++ C#/Sample.cs
@@ -4,12 +4,11 @@ using System.Linq;
 class Program {
 
     static void Main(string[] args) {
-        int[] numbers = {7, 5, 3, 13};
-        foreach(var item in from n in numbers orderby n select n) {
-            Console.WriteLine(item);
-        }
-        foreach(var item in numbers.OrderBy(c=>c)) {
-            Console.WriteLine(item);
-        }
+        int[] weights = {7,12,5,9,17};
+        var weightsOver10kg = weights.Where(c => c >= 10);
+        Console.WriteLine("10kg以上で最軽量の石は{0}kg", weightsOver10kg.Min());
+        Console.WriteLine("10kg以上で最重量の石は{0}kg", weightsOver10kg.Max());
+        Console.WriteLine("10kg以上で石の重さの平均は{0}kg", weightsOver10kg.Average());
+        if (weights.Any(c => c == 12)) Console.WriteLine("重さ12kgの石は存在する");
     }
 }
