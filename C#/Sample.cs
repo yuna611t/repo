9271277483b33520586c97922d0e6ee23e0b2b2c
naganuma <naganuma@192.168.11.12>
2015-11-15 22:18:31 +0900

    C#の勉強 参照渡し

--- C#/Sample.cs
+++ C#/Sample.cs
@@ -1,27 +1,14 @@
 using System;
 
-public class A { public int X; }
-public struct B { public int X; }
-
-
 class Program {
 
+    private static void twoitems(ref int a) {
+        a = a * 2;
+    }
+
     static void Main(string[] args) {
-        A a1 = new A();
-        B b1 = new B();
-        a1.X = 123;
-        b1.X = 123;
-
-        A a2 = a1; // 参照先のコピー(class)
-        B b2 = b1; // 値のコピー(struct)
-
-        a2.X = 456;
-        b2.X = 456;
-
-        Console.WriteLine("a1.X={0}", a1.X);
-        Console.WriteLine("a2.X={0}", a2.X);
-
-        Console.WriteLine("b1.X={0}", b1.X);
-        Console.WriteLine("b2.X={0}", b2.X);
+        int a = 2;
+        twoitems(ref a);
+        Console.WriteLine(a);
     }
 }
