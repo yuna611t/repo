10f5bf8b324103b77885d475f10e18a6ed277313
naganuma <naganuma@192.168.11.12>
2015-11-15 18:20:09 +0900

    C#の勉強 @を使用して予約語のシンボル化

--- C#/Sample.cs
+++ C#/Sample.cs
@@ -4,7 +4,8 @@ using System.Linq;
 class Program {
 
     static void Main(string[] args) {
-        int[] a = {0,1,2,3,4,5,6,7,8,9};
-        foreach(var item in a.Skip(2).Take(3)) Console.WriteLine(item);
+        // @を使えばシンボルとして使える
+        int @int = 4649;
+        Console.WriteLine(@int);
     }
 }
