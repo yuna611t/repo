c28ee76058437c6336608eb90c0817fde38f49a2
naganuma <naganuma@192.168.11.12>
2015-11-15 17:51:58 +0900

    C#の勉強 整数の計算と実数の計算

--- C#/Sample.cs
+++ C#/Sample.cs
@@ -1,14 +1,11 @@
 using System;
-using System.Linq;
 
 class Program {
 
     static void Main(string[] args) {
-        int[] weights = {7,12,5,9,17};
-        var weightsOver10kg = weights.Where(c => c >= 10);
-        Console.WriteLine("10kg以上で最軽量の石は{0}kg", weightsOver10kg.Min());
-        Console.WriteLine("10kg以上で最重量の石は{0}kg", weightsOver10kg.Max());
-        Console.WriteLine("10kg以上で石の重さの平均は{0}kg", weightsOver10kg.Average());
-        if (weights.Any(c => c == 12)) Console.WriteLine("重さ12kgの石は存在する");
+        // 整数の計算結果
+        Console.WriteLine("3/2={0}", 3 / 2);
+        // 実数の計算結果
+        Console.WriteLine("3.0/2.0={0}", 3.0 / 2.0);
     }
 }
