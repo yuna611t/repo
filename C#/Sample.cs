a84ba6520ff1f6b1a43132b50deee17ccc2da74a
naganuma <naganuma@192.168.11.12>
2015-11-15 18:11:14 +0900

    C#の勉強 Linqで中間だけの繰り返し

--- C#/Sample.cs
+++ C#/Sample.cs
@@ -1,9 +1,10 @@
 using System;
+using System.Linq;
 
 class Program {
 
     static void Main(string[] args) {
-        Console.WriteLine(3/2*2); // 3/2 => 1; 1*2 => 2
-        Console.WriteLine(3.0/2.0*2.0); 3.0/2.0 => 1.5; 1.5 * 2.0 => 3.0
+        int[] a = {0,1,2,3,4,5,6,7,8,9};
+        foreach(var item in a.Skip(2).Take(3)) Console.WriteLine(item);
     }
 }
