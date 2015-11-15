598ccb4fcf0c83145dc7af3fd21dd143553f4d88
naganuma <naganuma@192.168.11.12>
2015-11-15 18:28:57 +0900

    C#の勉強 オブジェクト初期化子

--- C#/Sample.cs
+++ C#/Sample.cs
@@ -1,11 +1,18 @@
 using System;
 using System.Linq;
 
+public class Place {
+    public string Name;
+    public string Type;
+}
+
 class Program {
 
     static void Main(string[] args) {
-        // @を使えばシンボルとして使える
-        int @int = 4649;
-        Console.WriteLine(@int);
+        // オブジェクト初期化子
+        var x = new Place(){Name = "東京", Type = "都"};
+        var y = new Place(){Name = "大阪", Type = "府"};
+        Console.WriteLine(x.Name + x.Type);
+        Console.WriteLine(y.Name + y.Type);
     }
 }
