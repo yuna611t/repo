11c066b28421d889748234e4ed6146a611d5def4
naganuma <naganuma@192.168.11.12>
2015-11-15 17:28:11 +0900

    C#の勉強 LinQでの数値加工

--- C#/Sample.cs
+++ C#/Sample.cs
@@ -1,33 +1,26 @@
 using System;
-using System.Collections.Generic;
 using System.Linq;
 
 class Program {
 
-    // LinQを使わない場合
-    private static string[] getANames(string[] names) {
-        var list = new List<string>();
-        foreach(var item in names) {
-            if (item.StartsWith("A")) list.Add(item);
-        }
-        return list.ToArray();
+    // LinQの値加工
+    private static int[] calc(int[] prices) {
+        return (from n in prices select n * 105/100).ToArray();
     }
-    // LinQの場合
-    private static string[] getANamesLinQ(string[] names) {
-        return (from n in names where n.StartsWith("A") select n).ToArray();
-    }
-    // LinQのメソッド形式
-    private static string[] getANamesLinQM(string[] names) {
-        return names.Where(c => c.StartsWith("A")).ToArray();
+    // LinQメッソド形式
+    private static int[] calc2(int[] prices) {
+        return prices.Select(c => c * 105/100).ToArray();
     }
 
     static void Main(string[] args) {
-        string[] names = { "Alice", "Anne", "Betty"};
-        string[] r = getANames(names);
-        string[] rL = getANamesLinQ(names);
-        string[] rM = getANamesLinQM(names);
-        foreach(var item in r) Console.WriteLine(item);
-        foreach(var item in rL) Console.WriteLine(item);
-        foreach(var item in rM) Console.WriteLine(item);
+        int[] prices = {100, 1000, 150};
+        var cP1 = calc(prices);
+        var cP2 = calc2(prices);
+        foreach(var item in cP1) {
+            Console.WriteLine(item);
+        }
+        foreach(var item in cP2) {
+            Console.WriteLine(item);
+        }
     }
 }
