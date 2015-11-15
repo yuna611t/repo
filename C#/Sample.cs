63fdd7a62026a622a19174152ad40293177a829c
naganuma <naganuma@192.168.11.12>
2015-11-15 17:18:30 +0900

    C#の勉強 LinQのメソッド形式

--- C#/Sample.cs
+++ C#/Sample.cs
@@ -12,17 +12,22 @@ class Program {
         }
         return list.ToArray();
     }
-
     // LinQの場合
     private static string[] getANamesLinQ(string[] names) {
         return (from n in names where n.StartsWith("A") select n).ToArray();
+    }
+    // LinQのメソッド形式
+    private static string[] getANamesLinQM(string[] names) {
+        return names.Where(c => c.StartsWith("A")).ToArray();
     }
 
     static void Main(string[] args) {
         string[] names = { "Alice", "Anne", "Betty"};
         string[] r = getANames(names);
         string[] rL = getANamesLinQ(names);
+        string[] rM = getANamesLinQM(names);
         foreach(var item in r) Console.WriteLine(item);
         foreach(var item in rL) Console.WriteLine(item);
+        foreach(var item in rM) Console.WriteLine(item);
     }
 }
