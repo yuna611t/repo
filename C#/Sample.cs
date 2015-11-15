b3ba2dd9897ab122bb4ece93f077dbda63aa5ac8
naganuma <naganuma@192.168.11.12>
2015-11-15 18:47:19 +0900

    C#の勉強 Linqによる値加工

--- C#/Sample.cs
+++ C#/Sample.cs
@@ -1,18 +1,12 @@
 using System;
 using System.Linq;
 
-public class Place {
-    public string Name;
-    public string Type;
-}
-
 class Program {
 
     static void Main(string[] args) {
-        // オブジェクト初期化子
-        var x = new Place(){Name = "東京", Type = "都"};
-        var y = new Place(){Name = "大阪", Type = "府"};
-        Console.WriteLine(x.Name + x.Type);
-        Console.WriteLine(y.Name + y.Type);
+        // 値の加工
+        string[] names = {"alice", "BETTY", "Cindy"};
+        var upperNames = names.Select(c => c.ToUpper());
+        foreach(var item in upperNames) Console.WriteLine(item);
     }
 }
