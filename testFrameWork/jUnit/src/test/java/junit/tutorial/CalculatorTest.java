246c6cb4ac5779889f019b6290995702f3dbad5e
naganuma <naganuma@192.168.11.12>
2015-12-19 19:02:08 +0900

    jUnitの勉強 jUnit実践入門 １章Tutorial 乗算テストメソッド作成

--- testFrameWork/jUnit/src/test/java/junit/tutorial/CalculatorTest.java
+++ testFrameWork/jUnit/src/test/java/junit/tutorial/CalculatorTest.java
@@ -1,12 +1,16 @@
 package junit.tutorial;
 
 import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
 import org.junit.Test;
 
 public class CalculatorTest {
 
 	@Test
 	public void multiplyで蒸散結果を取得できる() {
-		fail("まだ実装されていません");
+		Calculator calc = new Calculator();
+		int expected = 12;
+		int actual = calc.multiply(3, 4);
+		assertThat(actual, is(expected));
 	}
 }
