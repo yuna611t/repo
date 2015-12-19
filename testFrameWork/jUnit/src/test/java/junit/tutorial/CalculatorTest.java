3f1c9a35b6c076eb997558ce81146a0d4ab99849
naganuma <naganuma@192.168.11.12>
2015-12-19 19:05:24 +0900

    jUnitの勉強 jUnit実践入門 １章Tutorial 乗算テストメソッド追加(i件失敗)

--- testFrameWork/jUnit/src/test/java/junit/tutorial/CalculatorTest.java
+++ testFrameWork/jUnit/src/test/java/junit/tutorial/CalculatorTest.java
@@ -7,10 +7,18 @@ import org.junit.Test;
 public class CalculatorTest {
 
 	@Test
-	public void multiplyで蒸散結果を取得できる() {
+	public void multiplyで3と4の乗算結果を取得できる() {
 		Calculator calc = new Calculator();
 		int expected = 12;
 		int actual = calc.multiply(3, 4);
+		assertThat(actual, is(expected));
+	}
+	
+	@Test
+	public void multyplyで5と7の乗算結果を取得できる() {
+		Calculator calc = new Calculator();
+		int expected = 12;
+		int actual = calc.multiply(5, 7);
 		assertThat(actual, is(expected));
 	}
 }
