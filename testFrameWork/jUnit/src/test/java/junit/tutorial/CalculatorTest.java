d86f9a7f143bef15687bf8bce1c34a5a5270a00a
naganuma <naganuma@192.168.11.12>
2015-12-19 18:53:26 +0900

    jUnitの勉強 jUnit実践入門 UnitTestの雛形作成まで

---
+++ testFrameWork/jUnit/src/test/java/junit/tutorial/CalculatorTest.java
@@ -0,0 +1,12 @@
+package junit.tutorial;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class CalculatorTest {
+
+	@Test
+	public void test() {
+		fail("まだ実装されていません");
+	}
+}
