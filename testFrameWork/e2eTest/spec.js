018ed1d98129dd4a44861c6931560c09056c8f19
naganuma <naganuma@192.168.11.12>
2015-12-20 13:42:03 +0900

    E2ETestの勉強 Protractor  Demoをそのまま実行 Step4

--- testFrameWork/e2eTest/spec.js
+++ testFrameWork/e2eTest/spec.js
@@ -3,26 +3,23 @@ describe('Protractor Demo App', function() {
   var secondNumber = element(by.model('second'));
   var goButton = element(by.id('gobutton'));
   var latestResult = element(by.binding('latest'));
+  var history = element.all(by.repeater('result in memory'));
+
+  function add(a, b) {
+    firstNumber.sendKeys(a);
+    secondNumber.sendKeys(b);
+    goButton.click();
+  }
 
   beforeEach(function() {
     browser.get('http://juliemr.github.io/protractor-demo/');
   });
 
-  it('should have a title', function() {
-    expect(browser.getTitle()).toEqual('Super Calculator');
-  });
-
-  it('should add one and two', function() {
-    firstNumber.sendKeys(1);
-    secondNumber.sendKeys(2);
-
-    goButton.click();
-
-    expect(latestResult.getText()).toEqual('3');
-  });
-
-  it('should add four and six', function() {
-    // Fill this in.
-    expect(latestResult.getText()).toEqual('10');
+  it('should have a history', function() {
+    add(1, 2);
+    add(3, 4);
+    expect(history.count()).toEqual(2);
+    add(5, 6);
+    expect(history.count()).toEqual(3);
   });
 });
