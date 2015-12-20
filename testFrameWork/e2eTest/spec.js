95f651639c53a0df3e411259f1346758e900791c
naganuma <naganuma@192.168.11.12>
2015-12-20 13:17:11 +0900

    E2ETestの勉強 Protractor  成功するようにしたもの

--- testFrameWork/e2eTest/spec.js
+++ testFrameWork/e2eTest/spec.js
@@ -7,6 +7,6 @@ describe('Protractor Demo App', function() {
     element(by.id('gobutton')).click();
 
     expect(element(by.binding('latest')).getText()).
-        toEqual('5'); // This is wrong
+        toEqual('3'); // This is correct
   });
 });
