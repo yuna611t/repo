17e069f6887029b949018ca883eef6d2d755a16d
naganuma <naganuma@192.168.11.12>
2015-12-20 14:06:30 +0900

    log4jの勉強 各レベルのログ出力

--- logger/learning-log4j/src/main/java/learning/log4j/LearningLog4J.java
+++ logger/learning-log4j/src/main/java/learning/log4j/LearningLog4J.java
@@ -4,12 +4,18 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class LearningLog4J {
-
+	
 	private static final Logger logger = LogManager.getLogger("HelloWorld");
 
 	public static void main(String[] args) {
 		System.out.println("Hello World!!");
+
+		logger.trace("Hello, World");
+		logger.debug("Hello, World");
 		logger.info("Hello, World");
+		logger.warn("Hello, World");
+		logger.error("Hello, World");
+		logger.fatal("Hello, World");
 
 	}
 
