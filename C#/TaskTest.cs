a42bf3242c097bb9eb1655b8cf2e5d68d214f45e
naganuma <naganuma@192.168.11.12>
2016-02-07 21:22:29 +0900

    [mod] C＃の勉強 抽出方法の比較 - このサンプルプログラムでは、要素を全て集めてそこからPrallel.ForEachで抽出判定をかけるのが一番早かった

--- C#/TaskTest.cs
+++ C#/TaskTest.cs
@@ -10,21 +10,45 @@ public class Example
 {
     public static void Main()
     {
-        int size = 1000;
-        Item[] items = new Item[size];
-        for (int i = 0; i < size; i++)
-        {
-            Item item = null;
-            if (i % 300 == 0) {
-                items[i] = new Item(i, "new");
-            } else if(i % 2 == 0) {
-                items[i] = new Item(i, "mod");
-            } else {
-                items[i] = new Item(i, "del");
-            }
-            
-        }
+        extract00();
+        extract01();
+        extract02();
+    }
+    
+    static void extract00() {
+        Item[] items = createItemList(10000);
+                
+        System.Diagnostics.Stopwatch sw = new System.Diagnostics.Stopwatch();
+        sw.Start();
         
+        ArrayList array = new ArrayList();
+        
+        Console.WriteLine("----Before Loop----");
+        
+        foreach (Item item in items)
+        {
+            if (item.status == "new")
+            {
+                array.Add(item);                
+            }
+        }
+
+        Console.WriteLine("----After Loop----");
+
+        foreach (Item item in array)
+        {
+            Console.Write("new item is " + item.id + ", ");
+        }
+
+        sw.Stop();
+        Console.WriteLine("");
+        Console.WriteLine(sw.Elapsed);
+    }
+
+    
+    static void extract01() {
+        Item[] items = createItemList(10000);
+                
         System.Diagnostics.Stopwatch sw = new System.Diagnostics.Stopwatch();
         sw.Start();
         
@@ -36,20 +60,68 @@ public class Example
         IEnumerable<Item> records = from n in items where n.status == "new" select n;
         Parallel.ForEach(records,
             (x) => {
-                // if (x.status == "new") {
-                    lock (lockobj) { array.Add(x); }
-                // }
+                lock (lockobj) { array.Add(x); }
             });
 
           Console.WriteLine("----After Parallel----");
 
         foreach (Item item in array)
         {
-            Console.WriteLine("new item is " + item.id);
+            Console.Write("new item is " + item.id + ", ");
         }
 
         sw.Stop();
-        Console.WriteLine(sw.Elapsed);
+        Console.WriteLine("");
+        Console.WriteLine(sw.Elapsed);        
+    }
+    
+     static void extract02() {
+        Item[] items = createItemList(10000);
+                
+        System.Diagnostics.Stopwatch sw = new System.Diagnostics.Stopwatch();
+        sw.Start();
+        
+        ArrayList array = new ArrayList();
+        object lockobj = new object();
+        
+        Console.WriteLine("----Before Pallarel----");
+
+        IEnumerable<Item> records = from n in items select n;
+        Parallel.ForEach(records,
+            (x) => {
+                if (x.status == "new") {
+                    lock (lockobj) { array.Add(x); }
+                }
+            });
+
+          Console.WriteLine("----After Parallel----");
+
+        foreach (Item item in array)
+        {
+            Console.Write("new item is " + item.id + ", ");
+        }
+
+        sw.Stop();
+        Console.WriteLine("");
+        Console.WriteLine(sw.Elapsed);        
+    }
+    
+    static Item[] createItemList(int _size) {
+        int size = _size > 0 ? _size : 1000;
+        
+        Item[] items = new Item[size];
+        for (int i = 0; i < size; i++)
+        {
+            Item item = null;
+            if (i % 300 == 0) {
+                items[i] = new Item(i, "new");
+            } else if(i % 2 == 0) {
+                items[i] = new Item(i, "mod");
+            } else {
+                items[i] = new Item(i, "del");
+            }   
+        }
+        return items;
     }
 }
 
