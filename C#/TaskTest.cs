660edb032c42578cfa42e81e6d71434ef48b6f69
naganuma <naganuma@192.168.11.12>
2016-02-07 23:14:54 +0900

    [mod] C#の勉強 抽出方法の比較 ソースにコメントを入れる

--- C#/TaskTest.cs
+++ C#/TaskTest.cs
@@ -5,7 +5,9 @@ using System.Linq;
 using System.Collections;
 using System.Collections.Generic;
 
-
+/// <summary>
+/// データの抽出方法の違いによる速度の計測
+/// </summary>
 public class Example
 {
     public static void Main()
@@ -17,6 +19,9 @@ public class Example
     }
 }
 
+/// <summary>
+/// データ
+/// </summary>
 class Item
 {
     public int id { get; }
@@ -29,21 +34,43 @@ class Item
     }
 }
 
+
+  /// <summary>
+  /// データの抽出とその速度の計測を行う抽象クラス
+  /// </summary>
 abstract class Extract
 {
+    /// <summary>
+    /// データの抽出とその速度の計測を行う
+    /// </summary>
     public void extract() 
     {
-        Item[] items = createItemList(10000);    
+        // データの生成
+        Item[] items = createItemList(10000);
+        // 抽出時間の計測開始
         startTimer();                            
         Console.WriteLine("----Before Loop----");
+        // 抽出処理実行
         ArrayList array = correctItems(items);
         Console.WriteLine("----After Loop----");
+        // 抽出したデータの表示
         printExtractedItems(array);
+        // 抽出時間の表示
         stopTimer(); 
     }
     
+    /// <summary>
+    /// データの抽出を行う
+    /// <param name="items">データ抽出対象</param>
+    /// <returns>抽出されたデータ</returns>    
+    /// </summary> 
     protected abstract ArrayList correctItems(Item[] items);
-    
+
+    /// <summary>
+    /// データの生成を行う
+    /// <param name="_size">作成するデータの数</param>
+    /// <returns>作成されたデータ</returns>    
+    /// </summary>     
     protected Item[] createItemList(int _size) {
         int size = _size > 0 ? _size : 1000;
         
@@ -62,18 +89,29 @@ abstract class Extract
         return items;
     }
     
+    /// <summary>
+    /// 抽出されたデータの表示
+    /// <param name="array">表示するデータ</param>
+    /// </summary>     
     protected void printExtractedItems(ArrayList array) {
         foreach (Item item in array)
         {
             Console.Write("new item is " + item.id + ", ");
         }
     }
-    
-    System.Diagnostics.Stopwatch sw;
+
+    System.Diagnostics.Stopwatch sw;    
+    /// <summary>
+    /// 時間の計測開始
+    /// </summary>     
     protected void startTimer() {
         sw = new System.Diagnostics.Stopwatch();
-        sw.Start();        
+        sw.Start();       
     }
+    
+    /// <summary>
+    /// 時間の計測終了と計測時間の表示
+    /// </summary>     
     protected void stopTimer() {
         sw.Stop();
         Console.WriteLine("");
@@ -81,8 +119,17 @@ abstract class Extract
     }
 }
 
+/// <summary>
+/// データの抽出を直列に行う
+/// </summary> 
 class Extract00 : Extract
 {
+
+    /// <summary>
+    /// データの抽出を直列に行う
+    /// <param name="items">データ抽出対象</param>
+    /// <returns>抽出されたデータ</returns>    
+    /// </summary>
      protected override ArrayList correctItems(Item[] items) {
         ArrayList array = new ArrayList();
         foreach (Item item in items)
@@ -96,8 +143,18 @@ class Extract00 : Extract
     }
 }
 
+/// <summary>
+/// データの抽出を並列に行う
+/// 並列処理の対象を事前にLinQで集約しておく
+/// </summary> 
 class Extract01 : Extract
 {
+    /// <summary>
+    /// データの抽出を並列に行う
+    /// 並列処理の対象を事前にLinQで集約しておく
+    /// <param name="items">データ抽出対象</param>
+    /// <returns>抽出されたデータ</returns>    
+    /// </summary>
      protected override ArrayList correctItems(Item[] items) {
         ArrayList array = new ArrayList();
         object lockobj = new object();
@@ -110,8 +167,18 @@ class Extract01 : Extract
     }
 }
 
+/// <summary>
+/// データの抽出を並列に行う
+/// 並列処理の中でデータの抽出判定を行う
+/// </summary> 
 class Extract02 : Extract
 {
+    /// <summary>
+    /// データの抽出を並列に行う
+    /// 並列処理の中でデータの抽出判定を行う
+    /// <param name="items">データ抽出対象</param>
+    /// <returns>抽出されたデータ</returns>    
+    /// </summary>
      protected override ArrayList correctItems(Item[] items) {
         ArrayList array = new ArrayList();
         object lockobj = new object();
