e93b5370340fe206d88eee71a2fb59beb7d4b876
naganuma <naganuma@192.168.11.12>
2016-02-07 20:27:29 +0900

    [mod] C#の勉強 Parallel.ForEachを使用して要素の抽出

--- C#/TaskTest.cs
+++ C#/TaskTest.cs
@@ -1,28 +1,63 @@
 using System;
 using System.Threading;
 using System.Threading.Tasks;
+using System.Linq;
 using System.Collections;
+using System.Collections.Generic;
+
 
 public class Example
 {
     public static void Main()
     {
+        int size = 1000;
+        Item[] items = new Item[size];
+        for (int i = 0; i < size; i++)
+        {
+            Item item = null;
+            if (i % 300 == 0) {
+                items[i] = new Item(i, "new");
+            } else if(i % 2 == 0) {
+                items[i] = new Item(i, "mod");
+            } else {
+                items[i] = new Item(i, "del");
+            }
+            
+        }
+        
+        
         ArrayList array = new ArrayList();
         object lockobj = new object();
-        array.Add(100);
         
         Console.WriteLine("----Before Pallarel----");
-        Parallel.For(0, 10,
-            ctr => {
-                Console.WriteLine(ctr);
-                lock (lockobj) { array.Add(ctr); }
-            });
-        Console.WriteLine("----After Parallel----");
 
-        foreach (var item in array)
+        IEnumerable<Item> records = from n in items select n;
+        Parallel.ForEach(records,
+            (x) => {
+                if (x.status == "new") {
+                    lock (lockobj) { array.Add(x); }
+                }
+            });
+
+          Console.WriteLine("----After Parallel----");
+
+        foreach (Item item in array)
         {
-            Console.WriteLine(item);
+            Console.WriteLine("new item is " + item.id);
         }
 
     }
+}
+
+class Item
+{
+    public int id { get; }
+    public string status { get; set; }
+
+    public Item(int id, string status)
+    {
+        this.id = id;
+        this.status = status;   
+    }
+
 }
