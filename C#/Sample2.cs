ad71d24bfb28c6b4f049f228033057b2a12a63e8
naganuma <naganuma@192.168.11.12>
2016-07-18 18:58:44 +0900

    [new] C#の勉強 LinQの勉強

---
+++ C#/Sample2.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Linq;
+
+class SimpleQuery
+{
+    static void Main(string[] args)
+    {
+        int[] nums = { 1, -2, 3, 0, -4, 5 };
+
+        var posNums = from n in nums
+                      where n > 0
+                      select n;
+        
+        Console.WriteLine("The positive values in nums:");
+
+        foreach (int i in posNums) Console.WriteLine(i);
+    }
+}
