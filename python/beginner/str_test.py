180e8e2ce82dbbe8c4dba191432236280f007f9b
yuna <yuna611t@gmail.com>
2016-10-02 20:12:13 +0900

    format float number

--- python/beginner/str_test.py
+++ python/beginner/str_test.py
@@ -5,3 +5,8 @@ print(message)
 
 message = "{1}, {0}".format("Hello", "Good Morning")
 print(message)
+
+float_number = 10 / 3.14
+print(float_number)
+formated_float_number = "{:.3f}".format(float_number)
+print(formated_float_number)
