1a64122df8bed42f9bca43b2e6a2170344559627
yuna <yuna611t@gmail.com>
2016-10-02 17:08:06 +0900

    compare the reference of variable

--- python/beginner/main.py
+++ python/beginner/main.py
@@ -13,3 +13,19 @@ if __name__ == "__main__":
         print("-------------")
         print("object name is", obj)
         print("object id is", id(obj))
+
+    print("-------------")
+    print("check relationship of calculation and number variable")
+    n1 = 1
+    id_n1_bef = id(n1)
+    print("id of n1 is", id(n1))
+    n2 = 2
+    print("id of n2 is", id(n2))
+    n1 = n1 + n2
+    id_n1_aft = id(n1)
+    print("id of 'n1 = n1 + n2' is", id(n1))
+    print("--compare n1 which is before calculated with n1 which is after calculated --")
+    if id_n1_bef == id_n1_aft:
+        print("same")
+    else:
+        print("different")
