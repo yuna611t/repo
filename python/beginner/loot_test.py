db126c8b352df6981a98d0efbf43b78bbb7a82cf
yuna <yuna611t@gmail.com>
2016-10-02 19:48:09 +0900

    zip

--- python/beginner/loot_test.py
+++ python/beginner/loot_test.py
@@ -1,8 +1,12 @@
 # -*- coding: utf-8 -*- 
 
 source_list = ["Morning", "Evening", "Night", "Mid Night"]
+pair_list = ["朝", "昼", "夜", "深夜"]
 
 for index, element in enumerate(source_list):
     print("index is", index)
     print(" element is", element)
     print("")
+
+for english, japanese in zip(source_list, pair_list):
+    print(english, ":", japanese)
