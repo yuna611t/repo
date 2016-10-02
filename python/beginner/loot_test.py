91bdb28ff96a5a3df541d0e8318343aa4411ad60
yuna <yuna611t@gmail.com>
2016-10-02 19:44:30 +0900

    enumerate

---
+++ python/beginner/loot_test.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*- 
+
+source_list = ["Morning", "Evening", "Night", "Mid Night"]
+
+for index, element in enumerate(source_list):
+    print("index is", index)
+    print(" element is", element)
+    print("")
