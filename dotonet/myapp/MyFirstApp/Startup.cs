313a9580cdff454b8cf7bdc9f942abe103852410
yuna <yuna611t@gmail.com>
2016-10-10 13:15:52 +0900

    For a request to access image path

--- dotonet/myapp/MyFirstApp/Startup.cs
+++ dotonet/myapp/MyFirstApp/Startup.cs
@@ -1,11 +1,14 @@
 using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Logging;
 
 namespace MyFirstApp
@@ -48,6 +51,12 @@ namespace MyFirstApp
             }
 
             app.UseStaticFiles();
+            app.UseStaticFiles(new StaticFileOptions()
+            {
+                FileProvider = new PhysicalFileProvider(
+                    Path.Combine(Directory.GetCurrentDirectory(), @"MyStaticFiles")),
+                RequestPath = new PathString("/StaticFiles")
+            });
 
             app.UseMvc(routes =>
             {
