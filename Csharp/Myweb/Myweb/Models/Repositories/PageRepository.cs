04ad2d2f5625c467e557b37d0ef6068beb4ca42b
yuna <yuna611t@gmail.com>
2017-03-06 00:46:20 +0900

    Pageのデータを外だし

--- Csharp/Myweb/Myweb/Models/Repositories/PageRepository.cs
+++ Csharp/Myweb/Myweb/Models/Repositories/PageRepository.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using Newtonsoft.Json.Linq;
+
 namespace Myweb
 {
 	public class PageRepository : IPageRepository
@@ -6,21 +8,23 @@ namespace Myweb
 		public PageModel Find(int id)
 		{
 			// ナビゲーション取得
-			NavigationModel navigation = new NavigationRepository().Find(0);
+			NavigationModel navigation = new NavigationRepository().Find(id);
 
 			// 記事取得
-			ArticleModel article = new ArticleRepository().Find(0);
+			ArticleModel article = new ArticleRepository().Find(id);
 
 			// タブコンテンツ取得
-			TabsModel tabs = new TabsRepository().Find(0);
+			TabsModel tabs = new TabsRepository().Find(id);
 
-
+			// 一旦ここにハードコーディング
+			JToken contentToken = MyService.DBProvider.ContentsDB[id];
+			JToken pageToken = contentToken["PageContent"];
 
 			PageModel pageModel = new PageModel
 			{
-				Title = "Home Page",
-				SubTitle = "Sub Title",
-				Description = "This is description. <br> This is just a sample.",
+				Title = (string) pageToken["Title"],
+				SubTitle = (string)pageToken["SubTitle"],
+				Description = (string)pageToken["Description"],
 				Tabs = tabs,
 				Navigation = navigation,
 				Article = article					
