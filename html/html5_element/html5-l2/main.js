62e7ee1aff9f102704fc81406a38fb612df33697
yuna <yuna611t@gmail.com>
2017-04-30 17:01:26 +0900

    HTML5 L2 func

--- html/html5_element/html5-l2/main.js
+++ html/html5_element/html5-l2/main.js
@@ -1,18 +1,14 @@
-// 関数宣言と関数の巻き上げ
-console.log(add(1,2));
+var checkBMI = function(height, weight) {
+    var bmi = weight / (height * height);
+    console.log('BMI : ' + bmi);
 
-function add(a,b) {
-    return a + b;
+    if (bmi >= 25) {
+        console.log('Too fat');
+    } else if (bmi < 18.5) {
+        console.log('Too skiny');
+    } else {
+        console.log('Standard');
+    }
 }
 
-
-// 関数式
-try {
-    console.log(add2(1,2));
-} catch (e) {
-    console.log(e);
-}
-
-var add2 = function(a,b) {
-    return a + b;
-}
+checkBMI(1.76, 62)
