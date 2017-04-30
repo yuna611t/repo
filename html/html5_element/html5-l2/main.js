e3d4c8d01ae6a79701c30b8db33c61e7b33e6984
yuna <yuna611t@gmail.com>
2017-04-30 19:19:29 +0900

    HTML5 L2 DOM

--- html/html5_element/html5-l2/main.js
+++ html/html5_element/html5-l2/main.js
@@ -1,22 +1,4 @@
-var Person = function(name){
-    this.name = name;
-};
-Person.prototype.SayHello = function(){
-        console.log('Hello, ' + this.name + '!');
-};
-
-var Hero = function(name, level, job){
-    this.name = name;
-    this.level = level;
-    this.job = job;
-};
-
-Hero.prototype = new Person();
-Hero.prototype.SayStatus = function(){
-    console.log(this.name, ":", this.level, ":", this.job);
-};
-
-
-var hero1 = new Hero('Jhone', 2, 'Monk');
-hero1.SayHello();
-hero1.SayStatus();
+window.onload = function() {
+    var main = document.getElementById('main');
+    console.log(main);
+}
