404294bbfb4d003d7b685d6e97a4f53e1a465b79
yuna <yuna611t@gmail.com>
2017-04-30 18:27:42 +0900

    HTML5 L2 thisのメソッド呼び出し 関数呼び出し

--- html/html5_element/html5-l2/main.js
+++ html/html5_element/html5-l2/main.js
@@ -18,3 +18,17 @@ for(var key in person) {
         person[key]();
     }
 }
+
+var obj1 = {
+    f1: function(){
+        // thisのメソッド呼び出し => 自オブジェクト
+        console.log(this);
+    }
+}
+obj1.f1();
+
+var obj2 = function(){
+    // thisの関数呼び出し => windowオブジェクト
+    console.log(this);
+}
+obj2();
