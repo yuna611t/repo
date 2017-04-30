2532395e8bac51cc7b6acf0feb3b95c4bb202a64
yuna <yuna611t@gmail.com>
2017-04-30 18:36:08 +0900

    HTML5 L2 Constructor

--- html/html5_element/html5-l2/main.js
+++ html/html5_element/html5-l2/main.js
@@ -1,47 +1,9 @@
-var person = {
-    name: 'Tom',
-    sex: 'man',
-    email: 'dummy@dummy',
-    age: 32,
-    sayHello: function() {
+var Person = function(name){
+    this.name = name;
+    this.SayHello = function(){
         console.log('Hello, ' + this.name + '!');
     }
 };
 
-person.sayHello();
-
-for(var key in person) {
-    if (typeof(person[key]) !== 'function') {
-        console.log(key, ":", person[key]);
-    } else {
-        console.log(key, ":");
-        person[key]();
-    }
-}
-
-var obj = {
-    f1: function(){
-
-        var that = this;
-        console.log(that);
-        var fn = function(){
-            console.log(that);
-        }
-        fn();
-    }
-}
-obj.f1();
-
-var obj1 = {
-    f1: function(){
-        // thisのメソッド呼び出し => 自オブジェクト
-        console.log(this);
-    }
-}
-obj1.f1();
-
-var obj2 = function(){
-    // thisの関数呼び出し => windowオブジェクト
-    console.log(this);
-}
-obj2();
+var p1 = new Person('Jhon');
+p1.SayHello();
