9c6d33b9db54ec5e8f7b6e9b9f259f87e42e20ee
yuna <yuna611t@gmail.com>
2017-04-30 20:59:05 +0900

    HTML5 L2 addEventListener

--- html/html5_element/html5-l2/main.js
+++ html/html5_element/html5-l2/main.js
@@ -1,33 +1,15 @@
 window.onload = function() {
     var main = document.getElementById('main');
-    console.log(main);
+    
+    var element = document.createElement('p');
+    element.id = 'target';
+    element.innerText = 'Click Me!';
+    element.style.color = 'red';
+    main.appendChild(element);
 
-    var p = document.getElementsByTagName('p');
-    console.log(p);
+    var target = document.getElementById('target');
+    target.addEventListener('click', function() {
+        this.innerText = 'Clicked';
+    });
 
-    var parent = main.parentNode;
-    console.log(parent);
-
-    var firstChild = main.firstChild;
-    console.log(firstChild);
-    var firstElementChild = main.firstElementChild;
-    console.log(firstElementChild);
-
-    var lastChild = main.lastChild;
-    console.log(lastChild);
-    var lastElementChild = main.lastElementChild;
-    console.log(lastElementChild);
-
-    var children = main.children;
-    console.log(children);
-
-    var nextSibling = firstElementChild.nextSibling;
-    console.log(nextSibling);
-    var nextElementSibling = firstElementChild.nextElementSibling;
-    console.log(nextElementSibling);
-
-    var previousSibling = lastElementChild.previousSibling;
-    console.log(previousSibling);
-    var previousElementSibling = lastElementChild.previousElementSibling;
-    console.log(previousElementSibling);
-}
+};
