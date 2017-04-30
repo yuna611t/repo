f5d27cc118eb8c389f53fc1cc5845f213de3e316
yuna <yuna611t@gmail.com>
2017-04-30 18:57:37 +0900

    HTML5 L2 継承2

--- html/html5_element/html5-l2/main.js
+++ html/html5_element/html5-l2/main.js
@@ -9,17 +9,13 @@ var Hero = function(name, level, job){
     this.name = name;
     this.level = level;
     this.job = job;
-    this.SayStatus = function(){
-        console.log(this.name, ":", this.level, ":", this.job);        
-    }
 };
-/*
+
+Hero.prototype = new Person();
 Hero.prototype.SayStatus = function(){
     console.log(this.name, ":", this.level, ":", this.job);
 };
-*/
 
-Hero.prototype = new Person();
 
 var hero1 = new Hero('Jhone', 2, 'Monk');
 hero1.SayHello();
