da13b84e72006b800467e8d452406e7cfc939461
yuna <yuna611t@gmail.com>
2017-04-30 20:15:26 +0900

    HTML5 L2 create,Append

--- html/html5_element/html5-l2/form.js
+++ html/html5_element/html5-l2/form.js
@@ -1,7 +1,15 @@
 window.onload = function() {
+    var main = document.getElementById('main');
+
+    var element = document.createElement('p');
+    element.innerText = "Hello everyone!!";
+    main.appendChild(element);
+
+
     var forms = document.forms;
     console.log(forms);
     for (var i = 0; i < forms.length; i++) {
         console.log(forms[i]);
     }
+
 };
