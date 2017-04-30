7c1c74db1ab201185a21b580ce6b60ce7988d1a5
yuna <yuna611t@gmail.com>
2017-04-30 19:38:48 +0900

    HTML5 L2 parent child

--- html/html5_element/html5-l2/main.js
+++ html/html5_element/html5-l2/main.js
@@ -1,4 +1,22 @@
 window.onload = function() {
     var main = document.getElementById('main');
     console.log(main);
+
+    var p = document.getElementsByTagName('p');
+    console.log(p);
+
+    var parent = main.parentNode;
+    console.log(parent);
+
+    var firstChild = main.firstChild;
+    console.log(firstChild);
+    var firstElementChild = main.firstElementChild;
+    console.log(firstElementChild);
+
+    var lastChild = main.lastChild;
+    console.log(lastChild);
+    var lastElementChild = main.lastElementChild;
+    console.log(lastElementChild);
+
+
 }
