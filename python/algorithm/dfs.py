15f4378984199ac295fc114f1010287bad9b6bb4
yuna <yuna611t@gmail.com>
2017-09-04 02:00:25 +0900

    python algorithm print path by dfs

--- python/algorithm/dfs.py
+++ python/algorithm/dfs.py
@@ -1,20 +1,13 @@
-def dfs_paths(graph, start, goal):
-    # [('A', ['A'])]
-    stack = [(start, [start])]
-
-    while stack:
-        # stackの状態(1週目)  |_('A', ['A'])_|  => |_|
-        # stackの状態(2週目)  |_('C', ['A', 'C'])_|  => |_|      
-        (vertex, path) = stack.pop()
-        # 通過済みのノードを除いた現在の頂点に隣接するノードを処理する
-        for next in graph[vertex] - set(path):
-            if next == goal:
-                # 隣接するノードが目的地の場合、訪問済みパスに隣接するノードを追加してyeildにスタック
-                yield path + [next]
-            else:
-                # 隣接するノードが目的地でない場合、隣接するノードを頂点として隣接するノードまでのパスをStack
-                # stackの状態(1週目)  ||_|  => |_('C', ['A', 'C'])_|
-                stack.append((next, path + [next]))
+def dfs_paths(graph, start, goal, path=None):
+    if path is None:
+        path = [start]
+    if path == goal:
+        yield path
+    
+    yield path
+    for next_node in graph[start] - set(path):
+        for q in dfs_paths(graph, next_node, goal, path + [next_node]):
+            yield q
 
 
 if __name__ == '__main__':
@@ -25,8 +18,6 @@ if __name__ == '__main__':
         'D': set(['B']),
         'E': set(['B', 'F']),
         'F': set(['C', 'E'])}
-    
+
     # AからFまでの経路取得
-    paths = list(dfs_paths(graph, 'A', 'F'))
-    [print(path) for path in paths]
-   
+    [print(x) for x in dfs_paths(graph, 'A', 'F')]
